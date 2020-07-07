383e2672c53e66c316d9c3b0b996f9f68980f968
Abdul Rehman <mehars.6925@gmail.com>
2020-07-07 05:53:20 +0500

    gets data from the store

--- src/components/IbeRoomList.vue
+++ src/components/IbeRoomList.vue
@@ -1,124 +1,44 @@
 <template>
     <div class="w-full">
-        <IbeRoom :room=this.room1></IbeRoom>
+        <!-- <IbeRoom :room=this.room1></IbeRoom>
         <IbeRoom :room=this.room2></IbeRoom>
-        <IbeRoom :room=this.room3></IbeRoom>
+        <IbeRoom :room=this.room3></IbeRoom> -->
+
+        <div v-if="getRoomModuleBookProcessing">
+            Processing...
+        </div>
+
+        <div v-else-if="getRoomModuleBookError">
+            {{ getRoomModuleBookError }}
+        </div>
+
+        <div v-if="getRoomModuleFetchProcessing">
+            Processing....
+        </div>
+        <div v-else-if="getRoomModuleFetchError">
+            {{ getRoomModuleFetchError }}
+        </div>
+        <div v-else>
+            <IbeRoom v-for="(room, index) in getAllRooms" :key="index" :room="room"></IbeRoom>
+        </div>
 
     </div>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex'
     import IbeRoom from "./IbeRoom";
+
+
     export default {
         components: {
             IbeRoom
         },
-        data: function() {
-            return {
-                room1: {
-                    code: "STD",
-                    title: "Standard Room",
-                    summary: "Studio Apartment with an area of 41 m2. Max cap.: 2 adults + 1 child up to 2 years old",
-                    card_amenities: [
-                        {
-                            label: "Sleeps",
-                            value: 3
-                        },
-                        {
-                            label: "Beds",
-                            value: "2 Twin"
-                        },
-                        {
-                            label: "Area",
-                            value: "41 m²"
-                        },
-                    ],
-                    image: "background-image: url('/uploads/rooms/room-0.jpg')",
-                    rates: [
-                        {
-                            code: "BAR",
-                            title: "Best Available Rate",
-                            summary: "Hotel's Best Available Rate. Accommodation and all taxes included.",
-                            from: 190
-                        },
-                        {
-                            code: "NFR",
-                            title: "Non Refundable",
-                            summary: "Hotel's Non Refundable Rate. Accommodation and all taxes included.",
-                            from: 155
-                        }
-                    ]
-                },
-            room2: {
-                code: "DBL",
-                title: "Double Room",
-                summary: "Studio Apartment with an area of 41 m2. Max cap.: 2 adults + 1 child up to 2 years old",
-                card_amenities: [
-                    {
-                        label: "Sleeps",
-                        value: 2
-                    },
-                    {
-                        label: "Beds",
-                        value: "1 Twin"
-                    },
-                    {
-                        label: "Area",
-                        value: "21 m²"
-                    },
-                ],
-                image: "background-image: url('/uploads/rooms/room-1.jpg')",
-                rates: [
-                    {
-                        code: "BAR",
-                        title: "Best Available Rate",
-                        summary: "Hotel's Best Available Rate. Accommodation and all taxes included.",
-                        from: 120
-                    },
-                    {
-                        code: "NFR",
-                        title: "Non Refundable",
-                        summary: "Hotel's Non Refundable Rate. Accommodation and all taxes included.",
-                        from: 115
-                    }
-                ]
-            },
-            room3: {
-                code: "ST",
-                title: "Suite",
-                summary: "Studio Apartment with an area of 41 m2. Max cap.: 2 adults + 1 child up to 2 years old",
-                card_amenities: [
-                    {
-                        label: "Sleeps",
-                        value: 4
-                    },
-                    {
-                        label: "Beds",
-                        value: "2 Twin"
-                    },
-                    {
-                        label: "Area",
-                        value: "61 m²"
-                    },
-                ],
-                image: "background-image: url('/uploads/rooms/room-2.jpg')",
-                rates: [
-                    {
-                        code: "BAR",
-                        title: "Best Available Rate",
-                        summary: "Hotel's Best Available Rate. Accommodation and all taxes included.",
-                        from: 170
-                    },
-                    {
-                        code: "NRF",
-                        title: "Non Refundable",
-                        summary: "Hotel's Non Refundable Rate. Accommodation and all taxes included.",
-                        from: 175
-                    }
-                ]
-            }
-        }
-    },
+
+        computed: {
+            ...mapGetters(['getAllRooms', 'getRoomModuleFetchError', 'getRoomModuleFetchProcessing', 'getRoomModuleBookProcessing', 'getRoomModuleBookError'])
+        },
+
         name: 'IbeRoomList',
         props: {
 
