6a30ce1c63c6a3f56df985d2bf0ff3f7493dfc43
Abdul Rehman <mehars.6925@gmail.com>
2020-07-07 05:54:26 +0500

    now dispatches action along with data (for API call)

--- src/components/IbeSearchBar.vue
+++ src/components/IbeSearchBar.vue
@@ -2,32 +2,25 @@
     <div class="mx-auto hidden lg:block">
 
 
-        <form class="">
+        <form class="" @submit.prevent="search">
             <div class="flex flex-wrap -mx-3 mb-6 space">
                 <div class="w-full md:w-24 px-3 mb-6 md:mb-0">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-nr-adults">
                         Adults
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-nr-adults" type="number" name="nr_adults" placeholder="2">
+                    <input v-model="nr_adults" class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-nr-adults" type="number" name="nr_adults" placeholder="2">
                 </div>
                 <div class="w-full md:w-24 px-3 mb-6 md:mb-0">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-nr-children">
                         Children
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-nr-children" type="number" name="nr_children" placeholder="0">
+                    <input v-model="nr_child" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-nr-children" type="number" name="nr_children" placeholder="0">
                 </div>
-                <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-checkin">
-                        Check-In
+                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0 relative">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Date
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-checkin" type="date" name="checkin" placeholder="">
-                </div>
-                <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-checkout">
-                        Check-Out
-                    </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-checkout" type="date" name="checkout" placeholder="">
-
+                    <HotelDatePicker format="YYYY/MM/DD" class="absolute date-picker-custom" v-on:check-in-changed="checkInChanged" v-on:check-out-changed="checkOutChanged" />
                 </div>
 
                 <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
@@ -46,12 +39,58 @@
 </template>
 
 <script>
+    import HotelDatePicker from 'vue-hotel-datepicker'
+
     export default {
+        data() {
+            return {
+                nr_adults: "",
+                nr_child: "",
+                arrival_date: null,
+                departure_date: null,
+            }
+        },
+
         components: {
+            HotelDatePicker,
         },
         name: 'IbeSearchBar',
         props: {
 
+        },
+        methods: {
+            formatDate(date) {
+                let d = new Date(date),
+                    month = '' + (d.getMonth() + 1),
+                    day = '' + d.getDate(),
+                    year = d.getFullYear();
+
+                if (month.length < 2) 
+                    month = '0' + month;
+                if (day.length < 2) 
+                    day = '0' + day;
+
+                return [year, month, day].join('-');
+            },
+
+            checkInChanged (value) {
+                console.log(value)
+                this.arrival_date = this.formatDate(value)
+                document.querySelector('.datepicker__input:last-child').style.color = '#6acc9d'
+            },
+
+            checkOutChanged (value) {
+                this.departure_date = this.formatDate(value)
+            },
+            
+            search() {
+                if (this.nr_adults == "" || 
+                    this.nr_child == "" || 
+                    this.arrival_date == null || 
+                    this.departure_date == null) return alert('Please fill in all the fields')
+
+                this.$store.dispatch('searchRooms', this.$data)
+            }
         }
     }
 </script>
